6a5c0211b22dcbb3da9f28ab00480de1599a591f
Parker Chan <archer102125220.2015@gmail.com>
2023-03-16 11:11:55 +0800

    format

---
+++ .eslintrc.js
@@ -0,0 +1,93 @@
+// prettier-ignore
+module.exports = {
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    babelOptions: {
+      parserOpts: {
+        plugins: [
+          'jsx'
+        ]
+      }
+    }
+  },
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'react', 'import', 'jsdoc'],
+  extends: [
+    'eslint:recommended',
+    // 'plugin:@next/next/recommended',
+    // 'next/core-web-vitals',
+    'next',
+    'prettier'
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  // overrides: [
+  //   {
+  //     files: ['*.js'],
+  //     rules: {
+  //       'react-hooks/exhaustive-deps': 'off',
+  //       'react/prop-types': 'warn',
+  //       'import/no-anonymous-default-export': 'warn',
+  //       'no-extra-semi': 'warn',
+  //       'semi': [
+  //         'warn',
+  //         'always'
+  //       ],
+  //       'quotes': [
+  //         'error',
+  //         'single'
+  //       ],
+  //       'prefer-const': 'warn',
+  //       'no-var': 'warn',
+  //       'no-console': 'off',
+  //       'no-const-assign': 'warn',
+  //       'no-unused-vars': [
+  //         'warn',
+  //         {
+  //           'vars': 'all',
+  //           'args': 'after-used'
+  //         }
+  //       ],
+  //       'no-irregular-whitespace': 'warn',
+  //       'no-trailing-spaces': 'warn',
+  //       'no-undef': 'warn'
+  //     }
+  //   }
+  // ],
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    'react/prop-types': 'warn',
+    'import/no-anonymous-default-export': 'warn',
+    'no-extra-semi': 'warn',
+    'semi': [
+      'warn',
+      'always'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'prefer-const': 'warn',
+    'no-var': 'warn',
+    'no-console': 'off',
+    'no-const-assign': 'warn',
+    'no-unused-vars': [
+      'warn',
+      {
+        'vars': 'all',
+        'args': 'after-used'
+      }
+    ],
+    'no-irregular-whitespace': 'warn',
+    'no-trailing-spaces': 'warn',
+    'no-undef': 'warn'
+  }
+};
