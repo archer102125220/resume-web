baca5731d0148786f6c44e028504582699d53573
Parker Chan <archer102125220.2015@gmail.com>
2023-04-18 15:02:27 +0800

    open app test

--- pages/api/open-app-test/index.js
+++ pages/api/open-app-test/index.js
@@ -1,3 +1,4 @@
 export default function openAppTest(req, res) {
-  res.status(200).send('open app');
+  // res.status(200).send('open app');
+  res.status(200).json(JSON.parse(process.env.OPEN_IOS_APP || '{}'));
 }
