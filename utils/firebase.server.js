25c10aced23b84fda786e6e4c041c5967e7d588a
Parker Chan <archer102125220.2015@gmail.com>
2023-04-21 12:59:55 +0800

    add androidFirebase(firebase-admin)

--- utils/firebase.server.js
+++ utils/firebase.server.js
@@ -1,12 +1,22 @@
 export const credential = process.env.FIREBASE_CREDENTIAL || '{}';
 
 export const firebaseConfig = {
+  authDomain: 'resume-web-bcdbb.firebaseapp.com',
+  projectId: 'resume-web-bcdbb',
+  storageBucket: 'resume-web-bcdbb.appspot.com'
+};
+
+export const androidCredential =
+  process.env.ANDROID_FIREBASE_CREDENTIAL || '{}';
+
+export const androidFirebaseConfig = {
   authDomain: 'httpsbibiancojp.firebaseapp.com',
   projectId: 'httpsbibiancojp',
   storageBucket: 'httpsbibiancojp.appspot.com'
 };
 
 export let firebaseApp;
+export let androidFirebaseApp;
 
 export async function firebaseServerInit() {
   try {
@@ -16,6 +26,15 @@ export async function firebaseServerInit() {
         ...firebaseConfig,
         credential: firebaseAdmin.credential.cert(JSON.parse(credential))
       });
+      androidFirebaseApp = firebaseAdmin.initializeApp(
+        {
+          ...androidFirebaseConfig,
+          credential: firebaseAdmin.credential.cert(
+            JSON.parse(androidCredential)
+          )
+        },
+        'androidFirebase'
+      );
     }
   } catch (error) {
     console.log(error);
