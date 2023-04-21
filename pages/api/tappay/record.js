a9cacff438c3c9ab362d8feb81e4b2ee998b41b4
Parker Chan <archer102125220.2015@gmail.com>
2023-04-21 15:55:34 +0800

    tappay api error handle

--- pages/api/tappay/record.js
+++ pages/api/tappay/record.js
@@ -1,18 +1,22 @@
 import { POST_Record } from '@serverServices/tappay';
 
 export default async function record(req, res) {
-  const method = req.method.toLocaleUpperCase();
-  if (method !== 'POST') {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${method} Not Allowed`);
-    return;
-  }
-  const { body } = req;
-  const tappayResult = await POST_Record(body);
-  console.log({ tappayResult });
-  if (tappayResult.status === 0) {
-    res.status(200).json(tappayResult);
-  } else {
-    res.status(400).json(tappayResult);
+  try {
+    const method = req.method.toLocaleUpperCase();
+    if (method !== 'POST') {
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      return;
+    }
+    const { body } = req;
+    const tappayResult = await POST_Record(body);
+    console.log({ tappayResult });
+    if (tappayResult.status === 0) {
+      res.status(200).json(tappayResult);
+    } else {
+      res.status(400).json(tappayResult);
+    }
+  } catch (error) {
+    res.status(500).json(error);
   }
 }
--- pages/api/tappay/refund.js
+++ pages/api/tappay/refund.js
@@ -1,18 +1,22 @@
 import { POST_Refund } from '@serverServices/tappay';
 
 export default async function refund(req, res) {
-  const method = req.method.toLocaleUpperCase();
-  if (method !== 'POST') {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${method} Not Allowed`);
-    return;
-  }
-  const { body } = req;
-  const tappayResult = await POST_Refund(body);
-  console.log({ tappayResult });
-  if (tappayResult.status === 0) {
-    res.status(200).json(tappayResult);
-  } else {
-    res.status(400).json(tappayResult);
+  try {
+    const method = req.method.toLocaleUpperCase();
+    if (method !== 'POST') {
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      return;
+    }
+    const { body } = req;
+    const tappayResult = await POST_Refund(body);
+    console.log({ tappayResult });
+    if (tappayResult.status === 0) {
+      res.status(200).json(tappayResult);
+    } else {
+      res.status(400).json(tappayResult);
+    }
+  } catch (error) {
+    res.status(500).json(error);
   }
 }
