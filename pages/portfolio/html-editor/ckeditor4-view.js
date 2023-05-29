1dca7bd979d92248781033abc40f3431955001e4
Parker Chan <archer102125220.2015@gmail.com>
2023-05-29 09:16:30 +0800

    ckeditor4 view update

--- pages/portfolio/html-editor/ckeditor4-view.js
+++ pages/portfolio/html-editor/ckeditor4-view.js
@@ -1,14 +1,8 @@
-import { useState, useEffect, useRef, use } from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
-import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import Button from '@mui/material/Button';
 import { makeStyles } from '@mui/styles';
-
-const CKEditor = dynamic(
-  () => import('ckeditor4-react').then(({ CKEditor }) => CKEditor),
-  { ssr: false }
-);
 
 import { buttonStyle } from '@/styles/buttonStyle';
 import useGTMTrack from '@/hooks/useGTMTrack';
@@ -36,7 +30,6 @@ const styles = {
 const useStyles = makeStyles(styles);
 
 function CKEditor4View() {
-  const [CKEDITOR, set_CKEDITOR] = useState(null);
   const [title, setTitle] = useState('');
   const [h1Title, setH1Title] = useState('');
   const [h2Title, setH2Title] = useState('');
@@ -44,7 +37,6 @@ function CKEditor4View() {
   const [context, setContext] = useState('');
   const nextRouter = useRouter();
 
-  const CKEditorBlockRef = useRef(null);
   const classes = useStyles();
 
   useGTMTrack({
@@ -76,19 +68,11 @@ function CKEditor4View() {
     if (div.querySelector('h2') !== null) {
       setH2Title(div.querySelector('h2').innerText);
     }
-    if (typeof CKEDITOR?.instances?.editor1?.setData === 'function') {
-      CKEDITOR.instances.editor1.setData(context, {
-        // callback(a) {
-        //   console.log(a);
-        // }
-      });
-      console.log(context);
-      console.log({ CKEDITOR, context });
-    }
-  }, [context, CKEDITOR]);
+  }, [context]);
 
   function handleGoBack() {
-    nextRouter.back();
+    // nextRouter.back();
+    nextRouter.push('/portfolio/html-editor/ckeditor4');
   }
 
   return (
@@ -114,106 +98,7 @@ function CKEditor4View() {
       </div>
       <div className={classes.row}>
         <label>文章內文： </label>
-        <div ref={CKEditorBlockRef}>
-          <CKEditor
-            readOnly={true}
-            editorUrl="https://cdn.ckeditor.com/4.21.0/full-all/ckeditor.js"
-            onNamespaceLoaded={_CKEDITOR => {
-              _CKEDITOR.plugins.addExternal(
-                'videoembed',
-                '/ckeditor/plugins/videoembed/plugin.js'
-              );
-              set_CKEDITOR(_CKEDITOR);
-            }}
-            initData={context}
-            config={{
-              toolbar: [],
-              language: 'zh',
-              filebrowserUploadUrl: '/upload-img?a',
-              plugins: [
-                'dialogui',
-                'dialog',
-                'format',
-                'basicstyles',
-                'button',
-                'toolbar',
-                'fakeobjects',
-                'link',
-                'popup',
-                'undo',
-                'contextmenu',
-                'floatpanel',
-                'wysiwygarea',
-                'resize',
-                'elementspath',
-                'image',
-                'xml',
-                'ajax',
-                'filetools',
-                'filebrowser',
-                'notificationaggregator',
-                'notification',
-                'widgetselection',
-                'widget',
-                'uploadwidget',
-                'uploadimage',
-                'htmlwriter',
-                'videoembed',
-                'panel',
-                'menu',
-                'enterkey',
-                'entities',
-                'floatingspace',
-                'listblock',
-                'richcombo',
-                'pastetools',
-                'pastefromgdocs',
-                'pastefromlibreoffice',
-                'menubutton',
-                'showborders',
-                'lineutils',
-                'colorbutton',
-                'font'
-                // 'stylescombo'
-              ],
-              format_tags: 'p;h1;h2;h3;h4;h5;h6',
-              toolbarGroups: [
-                { name: 'document', groups: ['mode', 'document', 'doctools'] },
-                { name: 'clipboard', groups: ['clipboard', 'undo'] },
-                {
-                  name: 'editing',
-                  groups: ['find', 'selection', 'spellchecker']
-                },
-                { name: 'forms' },
-                { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
-                {
-                  name: 'paragraph',
-                  groups: ['list', 'indent', 'blocks', 'align', 'bidi']
-                },
-                { name: 'links' },
-                { name: 'insert' },
-                { name: 'styles' },
-                { name: 'colors' },
-                { name: 'tools' },
-                { name: 'others' },
-                { name: 'about' }
-              ],
-              removeButtons: [
-                'Cut',
-                'Copy',
-                'Paste',
-                'Undo',
-                'Redo',
-                'Anchor',
-                'Underline',
-                'Strike',
-                'Subscript',
-                'Superscript'
-              ],
-              removeDialogTabs: 'image:advanced;image:Link'
-            }}
-          />
-        </div>
+        <div dangerouslySetInnerHTML={{ __html: context }} />
       </div>
 
       <div className={classes.buttonBlock}>
