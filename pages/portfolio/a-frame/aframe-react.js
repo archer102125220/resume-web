b9932808f132688522ab16d2acfdc11e3f2b0bf5
Parker Chan <archer102125220.2015@gmail.com>
2023-06-06 17:45:16 +0800

    a-frame update

--- pages/portfolio/a-frame/aframe-react.js
+++ pages/portfolio/a-frame/aframe-react.js
@@ -49,7 +49,7 @@ function AFrameReact() {
               material={{ color: 'red' }}
               position={{ x: 0, y: 0, z: -5 }}
             />
-            <Entity particle-system={{ preset: 'snow' }} />
+            <Entity particle-system='preset: snow;' />
             <Entity light={{ type: 'point' }} />
             {/* <Entity gltf-model={{ src: 'virtualcity.gltf' }} /> */}
             <Entity
