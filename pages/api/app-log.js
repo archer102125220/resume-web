c344abe94f083bb36e0a47d801b1b8adfb8521cd
Parker <parker@jiapin.cloud>
2023-07-17 16:46:53 +0800

    log api update

--- pages/api/app-log.js
+++ pages/api/app-log.js
@@ -1,4 +1,15 @@
-export default function appLog(req, res) {
+import NextCors from 'nextjs-cors';
+
+export default async function appLog(req, res) {
+  // Run the cors middleware
+  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+  await NextCors(req, res, {
+     // Options
+     methods: ['POST'],
+     origin: '*',
+     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   const method = req.method.toLocaleUpperCase();
   if (method !== 'POST') {
     res.setHeader('Allow', ['POST']);
