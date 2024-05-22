3af465d81ad9569ff2689783392833be8cb883aa
Parker <parker@jiapin.cloud>
2024-05-22 16:20:27 +0800

    [fix]: gtm fix

--- pages/_document.js
+++ pages/_document.js
@@ -67,13 +67,13 @@ export default class NextDocument extends Document {
           {/* <Script src={'https://www.googletagmanager.com/gtag/js?id=' + (process.env.GA_ID || '')} async={true} id='dataLayer' strategy='beforeInteractive'></Script> */}
           <Script strategy="beforeInteractive" id="gtm">
             {`
-            // window.dataLayer = window.dataLayer || [];
-            // window.gtag = function (...arg) { window.dataLayer.push(arg); };
-            // window.gtag('js', new Date());
+            /* window.dataLayer = window.dataLayer || [];
+             window.gtag = function (...arg) { window.dataLayer.push(arg); };
+             window.gtag('js', new Date());
 
-            // window.gtag('config', '${process.env.GA_ID}', {
-            //   debug_mode: ${process.env.NODE_ENV === 'development'}
-            // });
+             window.gtag('config', '${process.env.GA_ID}', {
+               debug_mode: ${process.env.NODE_ENV === 'development'}
+             }); */
 
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${
               process.env.GTM_ID
