1f558762f249d935b373f78e7f66c8464601ba39
Parker Chen <archer102125220.2015@gmail.com>
2026-03-15 23:18:30 +0800

    fix: Add GTM ID fallback and update iframe style to JSX object.

--- pages/_document.js
+++ pages/_document.js
@@ -94,11 +94,11 @@ export default class NextDocument extends Document {
             <iframe
               src={
                 'https://www.googletagmanager.com/ns.html?id=' +
-                process.env.GTM_ID
+                (process.env.GTM_ID || '')
               }
               height="0"
               width="0"
-              style="display:none;visibility:hidden"
+              style={{ display: 'none', visibility: 'hidden' }}
             />
           </noscript>
           <Main />
