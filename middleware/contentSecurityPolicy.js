0672d8fb265a3a634d5ee338180ce4d515fc3e6d
Parker <archer102125220.2015@gmail.com>
2026-04-18 12:55:26 +0800

    feat: add content security policy middleware for production environments

--- middleware/contentSecurityPolicy.js
+++ middleware/contentSecurityPolicy.js
@@ -42,8 +42,8 @@ export async function contentSecurityPolicyMiddleware(request) {
     object-src 'none';
     script-src 'self' 'unsafe-inline' 'unsafe-eval' https://connect.facebook.net https://www.googletagmanager.com https://sandbox.tappaysdk.com https://portal.tappaysdk.com https://js.tappaysdk.com https://pay.google.com;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://js.tappaysdk.com;
-    connect-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com https://sandbox.tappaysdk.com https://portal.tappaysdk.com https://js.tappaysdk.com https://firebase.googleapis.com https://firebaseinstallations.googleapis.com https://fcmregistrations.googleapis.com https://analytics.google.com https://stats.g.doubleclick.net https://pay.google.com;
-    frame-src 'self' https://www.youtube.com https://www.googletagmanager.com https://sandbox.tappaysdk.com https://portal.tappaysdk.com https://js.tappaysdk.com https://*.tappaysdk.com https://pay.google.com;
+    connect-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com https://sandbox.tappaysdk.com https://portal.tappaysdk.com https://js.tappaysdk.com https://firebase.googleapis.com https://firebaseinstallations.googleapis.com https://fcmregistrations.googleapis.com https://analytics.google.com https://stats.g.doubleclick.net https://pay.google.com https://google.com;
+    frame-src 'self' https://www.youtube.com https://www.googletagmanager.com https://sandbox.tappaysdk.com https://portal.tappaysdk.com https://js.tappaysdk.com https://*.tappaysdk.com https://pay.google.com https://google.com;
     upgrade-insecure-requests;
 `;
 
